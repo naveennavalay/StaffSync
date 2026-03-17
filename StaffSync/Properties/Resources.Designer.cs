ce2f5f39f5bf63708b697082bb057d4250d10e69
NaveenDNavalay <naveendnavalay@gmail.com>
2026-03-18 01:24:01 +0530

    Additional Changes "Done"

--- StaffSync/Properties/Resources.Designer.cs
+++ StaffSync/Properties/Resources.Designer.cs
@@ -143,6 +143,36 @@ namespace StaffSync.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap asset_master {
+            get {
+                object obj = ResourceManager.GetObject("asset-master", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap asset_operations {
+            get {
+                object obj = ResourceManager.GetObject("asset-operations", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap assets {
+            get {
+                object obj = ResourceManager.GetObject("assets", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap attendance {
             get {
                 object obj = ResourceManager.GetObject("attendance", resourceCulture);
